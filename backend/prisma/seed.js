36c4d13adaa183842257c816bba507688aba4c83
mikolaj6r <mikolaj6r@gmail.com>
2021-03-02 15:56:03 +0100

    feat: add import data from imdb and seed it into our db

---
+++ backend/prisma/seed.js
@@ -0,0 +1,77 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+
+const path = require("path");
+const parse = require("csv-parse");
+const fetch = require("node-fetch");
+const zlib = require("zlib");
+
+async function downloadFile(url, path) {
+  const res = await fetch(url);
+  const fileStream = fs.createWriteStream(path);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on("error", reject);
+    fileStream.on("finish", resolve);
+  });
+}
+
+async function main() {
+  const filePath = path.join(__dirname, "..", "title.basics.tsv.gz");
+
+  await fsPromises
+    .stat(filePath)
+    .then((stats) => {
+      return fsPromises.unlink(filePath);
+    })
+    .catch();
+
+  await downloadFile(
+    "https://datasets.imdbws.com/title.basics.tsv.gz",
+    filePath
+  );
+
+  const source = fs.createReadStream(filePath);
+
+  const parser = parse({
+    delimiter: "\t",
+    columns: true,
+    quote: false,
+  });
+
+  source.pipe(zlib.createGunzip()).pipe(parser);
+
+  for await (const record of parser) {
+    const {
+      isAdult,
+      startYear,
+      endYear,
+      runtimeMinutes,
+      genres,
+      ...rest
+    } = record;
+
+    /* await prisma.movie.create({
+      data: {
+        ...rest,
+        isAdult: !!(isAdult === "1"),
+        startYear: parseInt(startYear, 10),
+        endYear: endYear === "\\N" ? null : parseInt(endYear, 10),
+        runtimeMinutes:
+          runtimeMinutes === "\\N" ? null : parseInt(runtimeMinutes, 10),
+      },
+    }); */
+
+    console.log(record.originalTitle);
+  }
+}
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
