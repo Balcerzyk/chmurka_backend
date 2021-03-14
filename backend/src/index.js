d5749c27019b5cefa186f10dc29e51f03fc91c8e
Michal <michal@localhost.localdomain>
2021-03-14 21:55:10 +0100

    page not found site added

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/movies", movies());
 app.use("/api/statistics", statistics());
-
+app.get("*", function(req, res){
+  res.status(404).send("page not found");
+});
 app.listen(8000, () => {
   console.log(`Server is listening on port: ${8000}`);
 });
